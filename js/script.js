7ad151d0bd24af7f3f749528a4ccc51f16bf051b
Igo Gomes <igogomes0@gmail.com>
2022-02-16 01:57:51 -0300

    Animações refatoradas e aplicadas através de dataset

--- js/script.js
+++ js/script.js
@@ -1,102 +1,13 @@
-function initTabNav() {
-    const tabMenu = document.querySelectorAll(".js-tabmenu li");
-    const tabContent = document.querySelectorAll(".js-tabcontent section");
-    tabContent[0].classList.add("ativo");
+const div = document.querySelector("div");
+const divColchete = document.querySelector("[data-cor]");
+const divColcheteValor = document.querySelector("[data-cor='azul']")
 
-    if(tabMenu.length && tabContent.length) {
-        function activeTab(index) {
-            tabContent.forEach((section) => {
-                section.classList.remove("ativo");
-            });
-            tabContent[index].classList.add("ativo");
-        }
+div.dataset.height = 1000;
 
-        tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener("click", () => {
-                activeTab(index);
-            });
-        });
-    }
-}
+delete div.dataset.width;
 
-initTabNav();
+div.dataset.totalHeight = 2000;
 
-function initAccordion() {
-    const accordionList = document.querySelectorAll(".js-accordion dt");
-    const activeClass = "ativo";
+console.log(div.dataset);
 
-    if(accordionList.length) {
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass);
-
-        function activeAccordion() {
-            this.classList.toggle(activeClass);
-            this.nextElementSibling.classList.toggle(activeClass);
-        }
-
-        accordionList.forEach((item) => {
-            item.addEventListener("click", activeAccordion);
-        }); 
-    }
-}
-
-initAccordion();
-
-function initScrollSuave() {
-    const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
-
-    function scrollToSection(e) {
-        e.preventDefault();
-        const href = e.currentTarget.getAttribute("href");
-        const section = document.querySelector(href);
-
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-
-        /* ALTERNATIVA
-        const topo = section.offsetTop;
-        window.scrollTo({
-            top: topo,
-            behavior: 'smooth'
-        });*/
-
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener("click", scrollToSection);
-    });
-}
-
-initScrollSuave();
-
-function initAnimacaoScroll() {
-    const sections = document.querySelectorAll(".js-scroll");
-
-    if(sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
-
-        function animaScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowMetade) < 0;
-                if(isSectionVisible) {
-                    section.classList.add("ativo");
-                }
-                else {
-                    //Alternativa para remoção de classe "ativo", reativando animação a todo momento. 
-                    //Caso a linha abaixo seja removida, a animação de exibição dos blocos de seções
-                    //será executada apenas uma vez.
-                    section.classList.remove("ativo");
-                }
-            });
-        }
-
-        animaScroll();
-
-        window.addEventListener("scroll", animaScroll);
-    }
-}
-
-initAnimacaoScroll();
+console.log("EXERCÍCIOS");
