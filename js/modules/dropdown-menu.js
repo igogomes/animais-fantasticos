2df210f4f949a0c3176ffc47609c9355af1dbc5a
Igo Gomes <igogomes0@gmail.com>
2022-03-18 02:48:52 -0300

    Refatoração para scripts relacionados à estrutura para o menu mobile

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -3,14 +3,16 @@ import outsideClick from "./outsideclick.js";
 export default class DropdownMenu {
     constructor(dropdownMenus, events) {
         this.dropdownMenus = document.querySelectorAll(dropdownMenus);
-        this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
-        this.activeClass = "active";
+
         // Definição de "touchstart" e "click" como argumentos padrões de events caso o usuário não defina
         if (events === undefined) {
             this.events = ["touchstart", "click"];
         } else {
             this.events = events;
         }
+
+        this.activeClass = 'active';
+        this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
     }
 
     //Ativação de dropdownmenu e adição de função para observação do clique fora do mesmo
@@ -19,7 +21,7 @@ export default class DropdownMenu {
         const element = e.currentTarget
         element.classList.add(this.activeClass);
         outsideClick(element, this.events, () => {
-            element.classList.remove(this.activeClass);
+            element.classList.remove("active");
         });
     }
 
@@ -27,7 +29,7 @@ export default class DropdownMenu {
     addDropdownMenusEvent() {
         this.dropdownMenus.forEach((menu) => {
            this.events.forEach((userEvent) => {
-                menu.addEventListener(userEvent, activeDropdownMenu);
+                menu.addEventListener(userEvent, this.activeDropdownMenu);
             });
         });
     }
