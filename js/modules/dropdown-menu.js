dc6c72c51742100258c16b6d50e407a0c9aa3c02
Igo Gomes <igogomes0@gmail.com>
2022-03-18 02:20:50 -0300

    Refatoração para scripts relacionados à estrutura para dropdown no menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,41 @@
 import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-    const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+export default class DropdownMenu {
+    constructor(dropdownMenus, events) {
+        this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+        this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+        this.activeClass = "active";
+        // Definição de "touchstart" e "click" como argumentos padrões de events caso o usuário não defina
+        if (events === undefined) {
+            this.events = ["touchstart", "click"];
+        } else {
+            this.events = events;
+        }
+    }
 
-    function handleClick(e) {
+    //Ativação de dropdownmenu e adição de função para observação do clique fora do mesmo
+    activeDropdownMenu(e) {
         e.preventDefault();
-        this.classList.add("active");
-        outsideClick(this, ["touchstart", "click"], () => {
-            this.classList.remove("active");
+        const element = e.currentTarget
+        element.classList.add(this.activeClass);
+        outsideClick(element, this.events, () => {
+            element.classList.remove(this.activeClass);
         });
     }
 
-    dropdownMenus.forEach((menu) => {
-        ["touchstart", "click"].forEach((userEvent) => {
-            menu.addEventListener(userEvent, handleClick);
+    // Adição de eventos ao dropdownmenu
+    addDropdownMenusEvent() {
+        this.dropdownMenus.forEach((menu) => {
+           this.events.forEach((userEvent) => {
+                menu.addEventListener(userEvent, activeDropdownMenu);
+            });
         });
-    });
+    }
+
+    init() {
+        if (this.dropdownMenus.length) {
+            this.addDropdownMenusEvent();
+        }
+        return this;
+    }
 }
