f9c89e5397ea768fca022f9d21a798674f965ad5
Igo Gomes <igogomes0@gmail.com>
2022-03-19 03:19:17 -0300

    Correção em método da classe destinada à exibição de informação relacionada ao horário de funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -29,7 +29,7 @@ export default class Funcionamento {
 
     init() {
         if (this.funcionamento) {
-            this.funcionamento();
+            this.dadosFuncionamento();
             this.dadosAgora();
             this.ativaAberto();
         }
